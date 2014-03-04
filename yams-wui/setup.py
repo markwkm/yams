2753be73a32594c2c5481e00deb13a407a45de4a
Mark Wong <markwkm@postgresql.org>
2014-03-03 21:28:45 -0800

    Install only pyramid 1.4 series
    The app needs to be updated for 1.5.  Will do that at a later time.

--- yams-wui/setup.py
+++ yams-wui/setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'pyramid',
+    'pyramid<1.5a',
     'SQLAlchemy',
     'transaction',
     'pyramid_tm',
